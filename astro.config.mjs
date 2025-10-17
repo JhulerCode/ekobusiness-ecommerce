343322b5ae290879b006a1d52d54320ec648239b
Jhuler Aguirre <jhuler.code@gmail.com>
2025-10-17 16:49:04 -0500

    fix: adaptador node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()]
