30504b1a6b2cc8d15e006cef92175cd3472b2ec2
Jhuler Aguirre <jhuler.code@gmail.com>
2025-10-17 17:01:55 -0500

    fix: adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/server'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()]
