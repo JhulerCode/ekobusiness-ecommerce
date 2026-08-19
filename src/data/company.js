ac461c6a24ac2742e98a2b268a3621e741b76c41
JhulerCode <jhuler.code@gmail.com>
2026-08-19 16:20:01 -0500

    feat: refactor company data handling by replacing API calls with static data import

---
+++ src/data/company.js
@@ -0,0 +1,56 @@
+export default {
+    ruc: '20557053213',
+    nombre: 'EKO BUSINESS S.A.C.',
+    nombre_comercial: 'Sunka Herbal Tea',
+    telefono: '(01) 432 865',
+    correo: 'comercial@sunkatea.com',
+
+    ecommerce_url: 'https://www.sunka.pe',
+    facebook_url: 'https://www.facebook.com/SunkaTea',
+    instagram_url: 'https://www.instagram.com/sunka.pe',
+    whatsapp_ventas: '+51 999 888 777',
+    whatsapp_ventas_url: 'https://wa.me/51999888777',
+
+    email_ventas: 'comercial@sunkatea.com',
+    email_soporte: 'soporte@sunkatea.com',
+    yape_number: '913621456',
+
+    igv_porcentaje: 18,
+    direcciones: [
+        {
+            id: 1,
+            nombre: 'PLANTA SANTA ANITA',
+            direccion: 'Cal.7 Mza. D Lote. 10 Urb. Los Productores',
+            ubigeo: 'Santa Anita - Lima - Lima',
+            principal: true,
+            datos_adicionales: {
+                referencia: 'Altura cuadra 4',
+            },
+        },
+        {
+            id: 2,
+            nombre: 'OFICINA',
+            direccion: 'Av. Mariscal La Mar 638',
+            distrito: 'Miraflores - Lima - Lima',
+            principal: false,
+        },
+    ],
+    bancos: [
+        {
+            id: 1,
+            nombre: 'SCOTIABANK PERU S.A.A.',
+            moneda: 'SOL',
+            nc: '000-1927574',
+            cci: '009-230-000001927574-42',
+            principal: true,
+        },
+        {
+            id: 2,
+            nombre: 'BANCO DE CRÉDITO DEL PERÚ - BCP',
+            moneda: 'SOL',
+            nc: '191-2611781-0-25',
+            cci: '002-191002611781-025-54',
+            principal: false,
+        },
+    ],
+}
